a2bb636f80c8c99700caf8e5009c8a5cc58391ea
lopinni <lopik98@gmail.com>
2022-12-13 10:20:30 +0100

    Added getting product by category

--- src/main/java/pl/britenet/campusapiapp/service/ProductService.java
+++ src/main/java/pl/britenet/campusapiapp/service/ProductService.java
@@ -62,6 +62,33 @@ public class ProductService {
         });
     }
 
+    public List<Product> getByCategory(int id) {
+        String sql = String.format(
+                "SELECT * FROM product p " +
+                        "JOIN productcategory pc ON p.id = pc.productid " +
+                        "WHERE pc.categoryid = %d",
+                id
+        );
+
+        return this.databaseService.performSQL(sql, resultSet -> {
+            try {
+                ArrayList<Product> productList = new ArrayList<>();
+                while (resultSet.next()) {
+                    productList.add(new ProductBuilder(new Product())
+                            .setId(resultSet.getInt("id"))
+                            .setName(resultSet.getString("name"))
+                            .setDescription(resultSet.getString("description"))
+                            .setImagePath(resultSet.getString("imagepath"))
+                            .setPrice(resultSet.getDouble("price"))
+                            .getProduct());
+                }
+                return productList;
+            } catch (SQLException e) {
+                throw new IllegalStateException(e);
+            }
+        });
+    }
+
     public List<List<Product>> getPaginated(int rownum) {
         String sql = "SELECT * FROM product";
         AtomicBoolean isRunning = new AtomicBoolean(true);
@@ -94,7 +121,8 @@ public class ProductService {
     public void insertProduct(Product product) {
         String dml = String.format(
                 Locale.US,
-                "INSERT INTO product (name, price, description, imagepath) VALUES ('%s', %f, '%s', '%s')",
+                "INSERT INTO product (name, price, description, imagepath) " +
+                        "VALUES ('%s', %f, '%s', '%s')",
                 product.getName(),
                 product.getPrice(),
                 product.getDescription(),
@@ -106,7 +134,12 @@ public class ProductService {
     public void updateProduct(Product product) {
         String dml = String.format(
                 Locale.US,
-                "UPDATE product SET name = '%s', price = %f, description = '%s', imagepath = '%s' WHERE id = %d",
+                "UPDATE product SET " +
+                        "name = '%s', " +
+                        "price = %f, " +
+                        "description = '%s', " +
+                        "imagepath = '%s' " +
+                        "WHERE id = %d",
                 product.getName(),
                 product.getPrice(),
                 product.getDescription(),
